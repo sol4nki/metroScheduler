74273d4d5af43ba2e0a578981f229de14224ea53
sol4nki <solankipranjal44@gmail.com>
2025-11-18 00:25:27 +0530

    few changes

--- DevLog.md
+++ DevLog.md
@@ -8,4 +8,4 @@ idea -> google fetch data? or use
 no idea, so working with banners as its the simplest and thinking of ideas 
 1. idea - adding graph to display metro properly (like use game logic refresh using os the terminal and display metro graph and active also switched similarly)
 2. using ANSI Shadow font for delhi metro text and making it look india themed too haha using color codes so like it looks like the flag -> https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Delhi%20METRO&x=none website used 
-3. 
+3. figured out the biggest headache, how to work with terminal uis so -> ill be using an amazing inbuilt module named shutil :D
--- banner.py
+++ banner.py
@@ -1,6 +1,9 @@
 # this file only contains the "big" chunks of text that ill 
 # use to make it aesthetically pleasing nothing mych
 
+import time # i m using this for almost everything this is the best module ever
+import os # using this to clear the screen after each load to make it look like an nimation
+
 def introduction():
     saffron = "\033[38;2;255;153;51m"
     white   = "\033[38;2;255;255;255m"
@@ -14,8 +17,8 @@ def introduction():
         ██╔══██╗██╔════╝██║     ██║  ██║██║    ████╗ ████║██╔════╝╚══██╔══╝██╔══██╗██╔═══██╗         
         ██║  ██║█████╗  ██║     ███████║██║    ██╔████╔██║█████╗     ██║   ██████╔╝██║   ██║         
 {white}        ██║  ██║██╔══╝  ██║     ██╔══██║██║    ██║╚██╔╝██║██╔══╝     ██║   ██╔══██╗██║   ██║         
-        ██████╔╝███████╗███████╗██║  {blue}██║██║    ██║ ╚═╝ ██{white}║███████╗   ██║   ██║  ██║╚██████╔╝         
-        ╚═════╝ ╚══════╝╚══════╝╚═╝  {blue}╚═╝╚═╝    ╚═╝     ╚═{white}╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝          
+        ██████╔╝███████╗███████╗██║  █{blue}█║██║    ██║ ╚═╝ ██{white}║███████╗   ██║   ██║  ██║╚██████╔╝         
+        ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚{blue}═╝╚═╝    ╚═╝     ╚═{white}╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝          
                                                                                                      
 
 ██╗    ██╗███████╗██╗      ██████╗ ███{blue}███╗ ███╗   ███╗███{white}████╗███████╗    ██╗   ██╗ ██████╗ ██╗   ██╗
@@ -28,7 +31,11 @@ def introduction():
     """
     return x
 
-def loading():
+def loading(i):
+    """
+    # using match so i can use time later on and then
+    # just print the dots (i couldnt figure out how to work with \n sequences in these)
+    """
     dot1 = """     
 ██╗
 ╚═╝     
@@ -41,9 +48,33 @@ def loading():
 ██╗ ██╗ ██╗
 ╚═╝ ╚═╝ ╚═╝
 """
-    return dot1
+    match i%3: 
+        
+        case 0:
+            return '\t\t\t' + dot1
+        case 1:
+            return '\t\t\t' + dot2
+        case 2:
+            return '\t\t\t' + dot3
+
+def clearScreen():
+    """
+    # I am using this because on reading os.system('clear') manual
+    # it states it has cls and clear diff for windows and linux and a solution
+    # on stackoverflow suggested to use nt so i am going with nt
+    """
+    if os.name == 'nt':  
+        os.system('cls')
+    else:  
+        os.system('clear')
 
 
 if __name__ == "__main__":
+    clearScreen() # just using this because i want a clear canvas
+    clearScreen() # twice because running only once leads to the dir path still being printed
     print(introduction())
-    # print(loading()*3)
+    time.sleep(1)
+    for i in range(10):
+        print(loading(i))
+        time.sleep(0.5)
+        clearScreen()
